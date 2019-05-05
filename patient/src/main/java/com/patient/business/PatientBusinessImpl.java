b0e93d0cad2dcbbc2538d36bdd9d3a03dfb00f4b
abeerjaloud <46846511+abeerjaloud@users.noreply.github.com>
2019-05-05 12:46:39 +0300

    Update PatientBusinessImpl.java

--- patient/src/main/java/com/patient/business/PatientBusinessImpl.java
+++ patient/src/main/java/com/patient/business/PatientBusinessImpl.java
@@ -79,4 +79,83 @@ public class PatientBusinessImpl implements PatientBusiness {
 
 		return response;
 	}
+public GetPatientResponse updatePatient(Patient patient) {
+		
+	    GetPatientResponse response = new GetPatientResponse();
+		BaseResponse baseResponse = new BaseResponse();
+		
+	try{
+		Patient PatientDb=patientDao.getOne(patient.getPatientId());
+		
+		System.out.println("We have patient ==> " + patient);
+		
+		PatientDb.setFirstName(patient.getFirstName());
+		PatientDb.setSecondName(patient.getSecondName());
+		PatientDb.setLastName(patient.getLastName());
+		PatientDb.setAge(patient.getAge());
+		PatientDb.setDateOfBirth(patient.getDateOfBirth());
+		PatientDb.setHeartRate(patient.getHeartRate());
+		PatientDb.setTemperature(patient.getTemperature());
+		PatientDb.setBloodPressure(patient.getBloodPressure());
+		
+		
+		System.out.println("Before saving");		
+		patientDao.save(PatientDb);
+		System.out.println("after saving");
+		
+		
+		baseResponse.setMessage("Patient ID:  "+PatientDb.getPatientId() + "Information successfully updated" );
+		baseResponse.setResultCode("SUCCESS");
+		baseResponse.setCode(1);
+		
+		
+	}
+	catch(Exception e){
+		System.out.println("Exception!!!!!");
+		baseResponse.setMessage(e.getMessage());
+		baseResponse.setResultCode("FAIL");
+		baseResponse.setCode(0);
+		e.printStackTrace();
+	}
+	
+		
+		response.setBaseResponse(baseResponse);
+		return response;
+	}
+
+public BaseResponse hardDeletePatient(int patientId) {
+	BaseResponse baseResponse = new BaseResponse();
+	try {
+		patientDao.delete(patientDao.getOne(patientId));
+		baseResponse.setMessage("Successfully deleted");
+		baseResponse.setResultCode("SUCCESS");
+		baseResponse.setCode(1);
+
+	} catch (Exception e) {
+		baseResponse.setMessage("Failed to delete the patient");
+		baseResponse.setResultCode("FAIL");
+		baseResponse.setCode(0);
+	}
+
+	return baseResponse;	
+}
+
+public BaseResponse softDeletePatient(int patientId) {
+	BaseResponse baseResponse = new BaseResponse();
+	try {
+		Patient patient = patientDao.getOne(patientId);
+		patient.setDeleted(0);
+		baseResponse.setMessage("Successfully deleted");
+		baseResponse.setResultCode("SUCCESS");
+		baseResponse.setCode(1);
+
+	} catch (Exception e) {
+		baseResponse.setMessage("Failed to delete the patient");
+		baseResponse.setResultCode("FAIL");
+		baseResponse.setCode(0);
+	}
+
+	return baseResponse;
+}
+
 }
